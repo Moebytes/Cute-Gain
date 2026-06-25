a8178c58172876efb9ddc5a871f6732487c41d3b
Moebytes <pie4chan@gmail.com>
2026-06-25 01:29:51 -0400

    binary

--- editor/BinaryData.h
+++ editor/BinaryData.h
@@ -11,6 +11,6 @@ namespace BinaryData
     const int           presets_zipSize = 6660;
 
     extern const char*  webview_files_zip;
-    const int           webview_files_zipSize = 3538477;
+    const int           webview_files_zipSize = 3538499;
 
 }
